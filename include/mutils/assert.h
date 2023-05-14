de4b0a7223d26ffd3e14b557e7a98ecf035c77da
Samir Bioud <samir@kronsy.dev>
2023-05-14 21:03:09 +0100

    Feat: Added more assertion macros for better value info
    Added macros which accept individual value parameters, so that we can
    individually print the value of each operand, so the developer may
    better debug the issue

--- include/mutils/assert.h
+++ include/mutils/assert.h
@@ -22,22 +22,60 @@
 
 #pragma once
 #include "./panic.h"
-
+#include "./stringify.h"
+#include <string>
 #ifndef NDEBUG
 
 // Debug mode is on
 #  define COMPTIME_STRINGIFY_DETAIL(x) #x
 #  define COMPTIME_STRINGIFY(x) COMPTIME_STRINGIFY_DETAIL(x)
 
-#  define MUTILS_ASSERT(expr, error)                                                                                   \
-    if (!(bool)(expr)) {                                                                                               \
-      mutils::PANIC(std::string("[[Assertion Error @ " __FILE__ ":" COMPTIME_STRINGIFY(__LINE__) "]] -> ") + error);   \
+#  define MUTILS_ASSERTION_ERROR(condition, ASSERTION_REASON, errmsg)                                                  \
+    if (!(condition)) {                                                                                                \
+      const auto message = std::string("[[Assertion Error @ " __FILE__ ":" COMPTIME_STRINGIFY(__LINE__) " ]] :: ") +   \
+                           std::string(ASSERTION_REASON) + "\n\t" + std::string(errmsg);                               \
+      mutils::PANIC(message);                                                                                          \
       exit(1);                                                                                                         \
     }
 
+#  define MUTILS_BINARY_ASSERTION_ERROR(a, b, operator_name, operator_symbol, errmsg)                                  \
+    auto a_eval = a;                                                                                                   \
+    auto b_eval = b;                                                                                                   \
+    MUTILS_ASSERTION_ERROR(                                                                                            \
+        a_eval operator_symbol b_eval,                                                                                 \
+        "MUTILS_ASSERT." operator_name "( " COMPTIME_STRINGIFY(a) ", " COMPTIME_STRINGIFY(b) " )  -> [" +              \
+            mutils::stringify(a_eval) + " " #operator_symbol " " + mutils::stringify(b_eval) + "] evaluated to false", \
+        errmsg);
+
+
+#  define MUTILS_ASSERT(expr, error)                                                                                   \
+    MUTILS_ASSERTION_ERROR((bool)(expr), "MUTILS_ASSERT.true " COMPTIME_STRINGIFY(expr), error)
+
+#  define MUTILS_ASSERT_NOT(expr, error)                                                                               \
+    MUTILS_ASSERTION_ERROR(!(bool)(expr), "MUTILS_ASSERT.false " COMPTIME_STRINGIFY(expr), error)
+
+#  define MUTILS_ASSERT_EQ(a, b, error)  MUTILS_BINARY_ASSERTION_ERROR(a, b, "equal", ==, error) 
+
+#  define MUTILS_ASSERT_NEQ(a, b, error) MUTILS_BINARY_ASSERTION_ERROR(a, b, "not_equal", !=, error)
+
+#define MUTILS_ASSERT_GT(a, b, error) MUTILS_BINARY_ASSERTION_ERROR(a, b, "greater", >, error)
+#define MUTILS_ASSERT_LT(a, b, error) MUTILS_BINARY_ASSERTION_ERROR(a, b, "lesser", <, error)
+
+#define MUTILS_ASSERT_GTE(a, b, error) MUTILS_BINARY_ASSERTION_ERROR(a, b, "greater_eq", >=, error)
+#define MUTILS_ASSERT_LTE(a, b, error) MUTILS_BINARY_ASSERTION_ERROR(a, b, "lesser_eq", <=, error)
 
 #else
 
 #  define MUTILS_ASSERT(expr, error)
+#  define MUTILS_ASSERT_NOT(expr, error)
+
+#  define MUTILS_ASSERT_EQ(a, b, error)
+#  define MUTILS_ASSERT_NEQ(a, b, error)
+
+#  define MUTILS_ASSERT_GT(a, b, error)
+#  define MUTILS_ASSERT_LT(a, b, error)
+
+#  define MUTILS_ASSERT_GTE(a, b, error)
+#  define MUTILS_ASSERT_LTE(a, b, error)
 
 #endif
